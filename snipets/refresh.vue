19e40ecde9499f845f892c190c8dab077ba50a08
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2022-10-25 18:38:18 +0900

    refresh , routerLeave 이벤트 감지

---
+++ snipets/refresh.vue
@@ -0,0 +1,24 @@
+<template>
+  <article>
+    <section>새로고침 감지 기능</section>
+  </article>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.addEventListener('beforeunload', this.unLoadEvent)
+  },
+  beforeUnmount() {
+    window.removeEventListener('beforeunload', this.unLoadEvent)
+  },
+  methods: {
+    unLoadEvent(event) {
+      event.preventDefault()
+      event.returnValue = ''
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ snipets/routerLeave.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>라우터 이동 감지 기능</article>
+</template>
+
+<script>
+export default {
+  // 페이지 이동 방지
+  // job/finished 페이지가 아니면 감지
+  beforeRouteLeave(to, from, next) {
+    if (to.path != '/job/finished') {
+      const text = '페이지 이동 시 입력사항이 삭제될 수 있습니다.\n그래도 이동하시겠습니까 ?'
+      if (confirm(text) == true) {
+        next()
+      } else next(false)
+    } else next()
+  }
+}
+</script>
+
+<style></style>
